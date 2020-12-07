679363b5d8244c845b9838cbde9782973e1348de
carzynsky <carzynski1@gmail.com>
2020-12-07 17:18:30 +0100

    Fix app bottom bar.

--- AppBottomBar.js
+++ AppBottomBar.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 import Home from './Home'
 import Pokemons from './Pokemons'
 import Favourites from './Favourites'
@@ -35,7 +36,45 @@ const Tab = createBottomTabNavigator();
 export default function AppBottomBar() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              if (route.name === 'Home') {
+                return (
+                  <MaterialCommunityIcons
+                    name={
+                      focused
+                        ? 'home-variant'
+                        : "home-variant-outline"
+                    }
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === 'Pokedex') {
+                return (
+                  <MaterialCommunityIcons
+                    name={'pokeball'}
+                    size={size}
+                    color={focused ? '#f93318' : 'gray'}
+                  />
+                );
+              }
+              else{
+                return (
+                  <AntDesign
+                    name={focused ? 'star' : 'staro'}
+                    size={size}
+                    color={focused ? 'yellow' : 'gray'}
+                  />
+                );
+              }
+            },
+          })}
+        tabBarOptions={{
+            activeTintColor: 'black',
+            inactiveTintColor: 'gray',
+          }}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Pokedex" component={PokemonsStackScreen} />
         <Tab.Screen name="Favourites" component={FavouriteStackScreen} />
